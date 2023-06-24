9fee74dbb6b21677b8a646397fc21f78a51db9be
racoltdev <racoltdev@gmail.com>
2023-06-24 11:54:12 -0400

    test macros run in local scope

--- src/test_macros.c
+++ src/test_macros.c
@@ -12,38 +12,7 @@ void printPipe(int* pipefd) {
 }
 
 int main() {
-	char* test_name = "Joe";
-	char* _raColTEST_my_name = STRING(test_name);
-	int _raColTEST_test_status = 0;
-	int _raColTEST_pipefd[2];
-	pipe(_raColTEST_pipefd);
-	if (!fork()) {
-		dup2(_raColTEST_pipefd[1], STDOUT_FILENO);
-		close(_raColTEST_pipefd[0]);
-		close(_raColTEST_pipefd[1]);
-		int conditional = 1;
-		char* details = "Main test function";
-		if (!conditional) {
-			printf(STRING(conditional) " %s", details);
-			return -1;
-		}
-		else {
-			printf("%s passed\n", test_name);
-		}
-		return 0;
-	}
-	else {
-		close(_raColTEST_pipefd[1]);
-		int _raColTEST_pid_status;
-		if (wait(&_raColTEST_pid_status)) {
-			_raColTEST_test_status = 0;
-			printPipe(_raColTEST_pipefd);
-			/* print_stacktrace;
-			   print captured_stdout;
-			log all data; \ */
-		}
-		else {
-			_raColTEST_test_status = 1;
-		}
-	}
+	TEST("First Test")
+	ASSERT(1 == 1, "This should pass")
+	END_TEST
 }
--- src/test_macros.h
+++ src/test_macros.h
@@ -8,23 +8,25 @@
 
 #define STRING(string) #string
 
-#define TEST(test_name) \
-	char* _raColTEST_my_name = #test_name; \
+#define TEST(_test_name) \
+{ \
+	char* raColTEST_test_name = _test_name; \
 	int _raColTEST_test_status = 0; \
 	int _raColTEST_pipefd[2]; \
 	pipe(_raColTEST_pipefd); \
 	if (!fork()) { \
-		dup2(_raColTEST_pipefd[1], STDOUT_FILENO)
+		dup2(_raColTEST_pipefd[1], STDOUT_FILENO);
 
 #define ASSERT(conditional, details) \
 	close(_raColTEST_pipefd[0]); \
 	close(_raColTEST_pipefd[1]); \
 	if (!conditional) { \
-		printf(STRING(conditional) " %s", details); \
+		printf("%s failed:\n\t", raColTEST_test_name); \
+		printf(STRING(conditional) "\n\t%s\n", details); \
 		return -1; \
 	} \
 	else { \
-		printf("%s passed\n", _raColTEST_my_name); \
+		printf("%s passed\n", raColTEST_test_name); \
 	} \
 
 #define END_TEST \
@@ -32,13 +34,14 @@
 	else { \
 		close(_raColTEST_pipefd[1]); \
 		int _raColTEST_pid_status; \
-		if (wait(&_raColTEST_pid_status) { \
+		if (wait(&_raColTEST_pid_status)) { \
 			_raColTEST_test_status = 0; \
 			printPipe(_raColTEST_pipefd); \
 			/* print_stacktrace; \ print captured_stdout; \ log all data; \ */ } \
 			else { \
 				_raColTEST_test_status = 1; \
 			} \
-	}
+	} \
+}
 
 void printPipe(int* pipefd);
