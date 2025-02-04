cbe6994f806f8881286707e597f4e45eccba2968
racoltdev <racoltdev@gmail.com>
2025-02-03 23:11:31 -0500

    trying using static var in config

--- src/lib_raColTest/config.cpp
+++ src/lib_raColTest/config.cpp
@@ -1,30 +1,31 @@
 #include "config.h"
 
+#include <cstddef>
+
 char* config::logfile_name() {
 	return "project.log";
 }
+
 int config::timeout() {
 	return 1;
 }
+
 char* config::test_source_dir() {
-	return "test";
+	static char* test_source_dir = "";
+	if (test_source_dir[0] == '\0') {
+		test_source_dir = "test";
+	}
+	return test_source_dir;
 }
+
 char* config::test_bin_dir() {
 	return "testbin";
 }
+
 bool config::enable_github_status() {
 	return false;
 }
+
 int config::verbosity() {
 	return 1;
-}
-
-int getConfigData(config::Config c) {
-	c.logfile_name = config::logfile_name();
-	c.test_source_dir = config::test_source_dir();
-	c.test_bin_dir = config::test_bin_dir();
-	c.verbosity = config::verbosity();
-	c.timeout = config::timeout();
-	c.enable_github_status = config::enable_github_status();
-	return 0;
 }
--- src/lib_raColTest/config.h
+++ src/lib_raColTest/config.h
@@ -2,23 +2,12 @@
 #define CONFIG_H
 
 namespace config {
-	struct Config {
-		char* logfile_name;
-		char* test_source_dir;
-		char* test_bin_dir;
-		int verbosity;
-		int timeout;
-		bool enable_github_status;
-	};
-
 	char* logfile_name();
 	int timeout();
 	char* test_source_dir();
 	char* test_bin_dir();
 	bool enable_github_status();
 	int verbosity();
-	int getConfigData(Config);
-
 }
 
 #endif
